527d7ff66d048bc7f7593daf59f28425be6958cc
sjachimowicz <stevejachimo@gmail.com>
2016-11-27 17:08:05 -0800

    stable version talking to sql server on my local

--- RapidoidTech/src/main/java/application/Main.java
+++ RapidoidTech/src/main/java/application/Main.java
@@ -3,17 +3,30 @@ package application;
 import org.rapidoid.annotation.GET;
 import org.rapidoid.annotation.Param;
 import org.rapidoid.annotation.Valid;
+import org.rapidoid.goodies.Goodies;
 import org.rapidoid.jpa.JPA;
 import org.rapidoid.setup.App;
 import org.rapidoid.setup.On;
+
+import application.entity.*;
 
 public class Main {
 
 	public static void main(String[] args) {
 	
-		App.bootstrap(args).jpa();
+		App.bootstrap("profiles=sqlserver").jpa();
+		
+		On.get("/").mvc(Goodies.config());
 		
 		 On.get("/books").json(() -> JPA.of(Book.class).all()); // get all books
+		
+		 On.get("/customer/all").json(() -> JPA.of(Customer.class).all());
+		 
+		 On.get("/employee/all").json(() -> JPA.of(Employee.class).all());
+		 		 
+		 On.get("/equipment/all").json(() -> JPA.of(Equipment.class).all());
+		 
+		 On.get("/ticket/all").json(() -> JPA.of(Ticket.class).all());
 		 
 	     On.post("/books").json((@Valid Book b) -> JPA.save(b)); // insert new book if valid
 	     
